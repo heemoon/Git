60359664e171cc80d5495765a063b9c3757d1ecc
heemoon <lixiwen0804@163.com>
2014-02-15 14:28:13 +0800

    xxxx
    xxxxxxx

--- FunCommon.h
+++ FunCommon.h
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-class MySocket;
-extern MySocket msServerListenSocket;
-extern volatile int nConnectionCount;
-extern volatile int nMaxConnectionCount;
-extern volatile UINT nListenPort;
 
-extern char cHostBasePath[255];
-extern char cHostName[100];
-extern char cDefaultPage[50];
-extern volatile bool bServerListenStatus;
-
-//lixiwen hahaha
-
-extern CFile* OpenFile(const char *pFileName);
-extern void ReadFile(const char *pFileName,char* pFileContext);
-extern void ReadConfigProperty(const char *pCPropertyName,char* pConfigPropertyValue);
-extern UINT ServerListenThread(LPVOID pParam);
-extern UINT HttpCommunicate(LPVOID pParam);
-=======
 class MySocket;
 extern MySocket msServerListenSocket;
 extern volatile int nConnectionCount;
@@ -35,4 +16,6 @@ extern void ReadFile(const char *pFileName,char* pFileContext);
 extern void ReadConfigProperty(const char *pCPropertyName,char* pConfigPropertyValue);
 extern UINT ServerListenThread(LPVOID pParam);
 extern UINT HttpCommunicate(LPVOID pParam);
->>>>>>> parent of d91e4f7... aaa
+
+
+//lixiwen
